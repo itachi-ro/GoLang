2fa476088036cac6da4573cead655f3b003c3872
rohan <rohannautiyal122000@gmail.com>
2025-05-20 16:49:07 +0530

    Read input with space

--- something.go
+++ something.go
@@ -1,10 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func do() {
 	var name string
-	fmt.Print("Enter your Name\n")
-	fmt.Scan(&name)
-	fmt.Print("Hello ", name)
+
+	fmt.Print("Enter your Name: \n")
+	fmt.Scan(&name) // Rohan Nautiyal
+
+	fmt.Printf("Hello, %v\n", name) // Hello, Rohan
 }
+
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// 	"strings"
+// )
+
+// reader := bufio.NewReader(os.Stdin)
+// var name string
+
+// fmt.Print("Enter your Name: \n")
+// name, _ = reader.ReadString('\n')
+// name = strings.TrimSpace(name)
+
+// fmt.Printf("Hello, %v\n", name)
